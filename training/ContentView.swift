676ac5d311e810e42d13ffb19a7bec581896f328
Taoufiq El Moutaouakil <rztaoufiq@gmail.com>
2024-08-26 09:49:54 -0700

    Initial Commit

---
+++ training/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  training
+//
+//  Created by Taooufiq El moutaoouakil on 8/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ training/trainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  trainingApp.swift
+//  training
+//
+//  Created by Taooufiq El moutaoouakil on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct trainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
