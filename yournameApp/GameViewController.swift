9e96e89352afe2f478adeb904712d5ba86ccbeb8
Taoufiq El Moutaouakil <rztaoufiq@gmail.com>
2024-09-03 07:32:26 -0700

    update some change

--- yournameApp/GameViewController.swift
+++ yournameApp/GameViewController.swift
@@ -14,25 +14,9 @@ class GameViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
 
 
-    @IBAction func usernameTextField(_ sender: UITextField) {
-    }
-    
-    @IBAction func passwordTextField(_ sender: UITextField) {
-    }
-    @IBAction func loginButtonPressed(_ sender: UIButton) {
-        print("test")
-        guard let username = usernameTextField.text, !username.isEmpty,
-                      let password = passwordTextField.text, !password.isEmpty else {
-                    print("Error: Username or Password cannot be empty.")
-                    return
-                }
-                
-                // Proceed with login logic
-                print("Logging in with username: \(username) and password: \(password)")
-    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("View did load")
     }
     }
 
