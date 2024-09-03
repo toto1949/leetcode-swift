26738e5de003e4c1086804aab8cf5116f1970512
Taoufiq El Moutaouakil <rztaoufiq@gmail.com>
2024-09-03 07:29:26 -0700

    create tab bar using srot board

--- yournameApp/GameViewController.swift
+++ yournameApp/GameViewController.swift
@@ -10,36 +10,30 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
 
+
+    @IBAction func usernameTextField(_ sender: UITextField) {
+    }
+    
+    @IBAction func passwordTextField(_ sender: UITextField) {
+    }
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        print("test")
+        guard let username = usernameTextField.text, !username.isEmpty,
+                      let password = passwordTextField.text, !password.isEmpty else {
+                    print("Error: Username or Password cannot be empty.")
+                    return
+                }
+                
+                // Proceed with login logic
+                print("Logging in with username: \(username) and password: \(password)")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        print("View did load")
+    }
     }
 
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
 
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
-}
